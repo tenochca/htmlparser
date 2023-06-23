98fb1c9afbf5c7f696eecef6a75f0d1814a580f4
Diego Izaguirre <tenochca.di@outlook.com>
2023-06-23 15:50:15 -0500

    commiting changes to prompt the user for URL and then a class to parse through the code. Returns the start tags and end tags plus the data inside those tags

--- htmlparser.py
+++ htmlparser.py
@@ -1,3 +1,31 @@
 
+from urllib.request import urlopen
+
+def gethtml(fullurl):
+    'issue http request, return the html code as a string'
+    r = urlopen(fullurl)
+    h = r.read().decode()
+    return h
+
+from html.parser import HTMLParser
+
+class simpleparser(HTMLParser):
+    'parser to get just the tags and data'
+
+    def handle_starttag(self, tag, attrs):
+        print('START TAG: ', tag)
+
+    def handle_endtag(self, tag):
+        print('END TAG: ', tag)
+
+    def handle_data(self, data):
+        d = data.strip()
+        print('***'+ d + '***')
+
+
+fullurl = input('URL: ')
+code = gethtml(fullurl)
+parser = simpleparser()
+parser.feed(code)
 
 
